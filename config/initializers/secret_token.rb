0b854680eddb05b41b5b8a3fea619fd4656e637b
Geoff Wild <boris@zerohour-productions.net>
2013-11-20 23:15:10 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewhockeyApp::Application.config.secret_key_base = '4ce4c848dca935ed95002b0ed8302995aa16b3308c562f3015c9f858b4c2664286c6dbad6d43ed8bc93ee43b45938c630ca4054122c71533ee109c8ce90d7947'
