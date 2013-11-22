9d9cf3a99acb031db662fa6d69e999c208dddda1
Geoff Wild <boris@zerohour-productions.net>
2013-11-22 15:59:31 -0600

    Added some more tests and started some dynamic content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,22 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+  	it "should have the title 'NEWHockey.com | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('NEWHockey.com | Home')
+    end
 
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Northeast Wisconsin Hockey'" do
       visit '/static_pages/home'
       expect(page).to have_content('Northeast Wisconsin Hockey')
     end
   end
 
   describe "Help page" do
+  	it "should have the correct title 'Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_title('NEWHockey.com | Help')
+  	end
 
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
@@ -19,6 +27,11 @@ describe "Static pages" do
   end	
 
   describe "About Page" do
+  	it "should have the correct title 'About'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title('NEWHockey.com | About')
+  	end
+
   	it "should have the content 'About'" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About')
