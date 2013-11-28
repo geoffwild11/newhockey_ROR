73c191f5151a9642d3a44518a68513b7d3cdc2b3
Geoff Wild <boris@zerohour-productions.net>
2013-11-27 22:11:46 -0600

    getting ready for filling up the layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "NEWHockey.com" }
+
   describe "Home page" do
   	it "should have the title 'NEWHockey.com | Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('NEWHockey.com | Home')
+      expect(page).to have_title("#{base_title}")
     end
 
     it "should have the content 'Northeast Wisconsin Hockey'" do
@@ -17,7 +19,7 @@ describe "Static pages" do
   describe "Help page" do
   	it "should have the correct title 'Help'" do
   		visit '/static_pages/help'
-  		expect(page).to have_title('NEWHockey.com | Help')
+  		expect(page).to have_title("#{base_title} | Help")
   	end
 
   	it "should have the content 'Help'" do
@@ -29,12 +31,24 @@ describe "Static pages" do
   describe "About Page" do
   	it "should have the correct title 'About'" do
   		visit '/static_pages/about'
-  		expect(page).to have_title('NEWHockey.com | About')
+  		expect(page).to have_title("#{base_title} | About")
   	end
 
   	it "should have the content 'About'" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About')
+  	end
+  end
+
+  describe "Contact Page" do
+  	it "should have the correct title 'Contact'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_title("#{base_title} | Contact")
+  	end
+
+  	it "should have the content 'Contact'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact')
   	end
   end
 
